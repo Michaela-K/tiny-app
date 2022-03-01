35d09ff4a11c53e485237664ea4ddd71bd6f0caf
Michaela-K <michaelakodes@gmail.com>
2022-03-01 02:35:29 +0000

    add redirect route

--- express_server.js
+++ express_server.js
@@ -24,6 +24,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//redirect links
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -34,6 +40,7 @@ app.get("/urls/new", (req, res) => {
 //is the urls/id the same as urls shortURL?
 
 // The : in front of id indicates that id is a route parameter. This means that the value in this part of the url will be available in the req.params object.
+//to show the user the newly created link
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
@@ -61,7 +68,7 @@ app.get("/", (req, res) => {
 //POST
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/:shortURL");         // Respond with 'Ok' (we will replace this)
 });
 
 
