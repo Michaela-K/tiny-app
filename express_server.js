171d0d5fbab7e938d5be384e8c7b798ed681e0ea
Michaela-K <michaelakodes@gmail.com>
2022-03-01 01:48:49 +0000

    add show page and route

--- express_server.js
+++ express_server.js
@@ -13,9 +13,19 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
 //GET
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+// The : in front of id indicates that id is a route parameter. This means that the value in this part of the url will be available in the req.params object.
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: req.params.longURL 
+  };
+  res.render("urls_show", templateVars);
 });
 
 //When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable
@@ -30,6 +40,8 @@ app.get("/urls", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+
 
 
 //POST
