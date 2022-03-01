7d97a21784880e205eb434aaff5ab2485f52b161
Michaela-K <michaelakodes@gmail.com>
2022-03-01 01:37:46 +0000

    add url page

--- express_server.js
+++ express_server.js
@@ -9,19 +9,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+//GET
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable
+app.get("/urls", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase 
+  };
+  res.render("urls_index", templateVars);
+});
+
 //HomePage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+//POST
