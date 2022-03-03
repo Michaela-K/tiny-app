ff381eaec647deb649bbc5594bbcbb9e0fc5506a
Michaela-K <michaelakodes@gmail.com>
2022-03-03 18:02:09 +0000

    modularise code - database and helper function files

--- express_server.js
+++ express_server.js
@@ -19,29 +19,15 @@ app.use(
   })
 );
 
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "Wh1ym3"
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: "M0rL0v"
-  }
-};
+const {
+  urlsForUser,
+  passwordChk,
+  hasUserId,
+  getUserByEmail,
+  generateRandomString,
+} = require("./helpers");
 
-const users = { 
-  "Wh1ym3": {
-    user_id: "Wh1ym3", 
-    email: "user@example.com", 
-    password: bcrypt.hashSync("password", 10)
-  },
- "M0rL0v": {
-    user_id: "M0rL0v", 
-    email: "user2@example.com", 
-    password: bcrypt.hashSync("password", 10)
-  }
-}
+const { urlDatabase, users } = require("./databases");
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -249,71 +235,3 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
-
-
-
-
-
-
-
-
-
-
-//HELPER FUNCTIONS
-function urlsForUser(user_id, urlDatabase) {
-  let userUrl = {};
-  for (const url in urlDatabase) {
-    if (urlDatabase[url].userID === user_id) {
-      userUrl[url] = urlDatabase[url].longURL;
-    }
-  }
-  return userUrl;
-}
-
-
-function passwordChk(email, password, users) {
-  for (const user in users) {
-    if (
-      users[user].email === email &&
-      bcrypt.compareSync(password, users[user].password)
-    ) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function hasUserId(email, users) {
-  for (const user in users) {
-    console.log("1", user.email)
-    console.log("2", users[user].email)
-    console.log("3", users[user][email])
-    console.log("4", users[user]["email"])
-
-    if (users[user]["email"] === email) {
-      return users[user].user_id;
-    }
-  }
-  return false;
-}
-
-function getUserByEmail(email, users) {
-  for (const user in users) {
-    if (users[user].email === email) {
-      return true;
-    }
-  }
-  return null;
-}
-
-function generateRandomString() {
-  let length = 6;
-  let result = "";
-  let characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
---
+++ helpers.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcryptjs");
+
+//HELPER FUNCTIONS
+function urlsForUser(user_id, urlDatabase) {
+  let userUrl = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === user_id) {
+      userUrl[url] = urlDatabase[url].longURL;
+    }
+  }
+  return userUrl;
+}
+
+
+function passwordChk(email, password, users) {
+  for (const user in users) {
+    if (
+      users[user].email === email &&
+      bcrypt.compareSync(password, users[user].password)
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function hasUserId(email, users) {
+  for (const user in users) {
+    console.log("1", user.email)
+    console.log("2", users[user].email)
+    console.log("3", users[user][email])
+    console.log("4", users[user]["email"])
+
+    if (users[user]["email"] === email) {
+      return users[user].user_id;
+    }
+  }
+  return false;
+}
+
+function getUserByEmail(email, users) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return true;
+    }
+  }
+  return null;
+}
+
+function generateRandomString() {
+  let length = 6;
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+module.exports = {
+  urlsForUser,
+  passwordChk,
+  hasUserId,
+  getUserByEmail,
+  generateRandomString,
+};
